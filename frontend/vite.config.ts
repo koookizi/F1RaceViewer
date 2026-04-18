e08ea476657eac6fb01078144fc38aac34890929
Albert <koookizi@outlook.com>
2026-04-18 13:16:52 +0100

    fix routing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/f1raceviewer/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
